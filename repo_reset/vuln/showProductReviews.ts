e6bdf3c0bea542a9964ce574d91ebfac9fa9247e
Austin Becker <austinjaybecker@github.com>
2026-02-27 10:59:47 -0700

    Update showProductReviews.ts

--- repo_reset/vuln/showProductReviews.ts
+++ repo_reset/vuln/showProductReviews.ts
@@ -1,6 +1,10 @@
 /*
  * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
+ kick off scan plz
+ 
+ 
+ 
  */
 
 import utils = require('../lib/utils')
